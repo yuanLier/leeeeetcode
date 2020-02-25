eba948131d149cf0e036bce707e963eab514d5d3
yuanlier <yuan15826770919@126.com>
2020-02-25 18:59:21 +0800

    20. 包含min函数的栈

---
+++ src/_20200225/Sooolution.java
@@ -0,0 +1,46 @@
+package _20200225;
+
+import java.util.Stack;
+
+/**
+ * @author yuanyiwen
+ * @create 2020-02-25 18:40
+ * @description 包含min函数的栈
+ *      题目描述 ：
+ *          定义栈的数据结构，
+ *          请在该类型中实现一个能够得到栈中所含最小元素的min函数
+ *          （时间复杂度应为O（1））。
+ *          注意：
+ *          保证测试中不会当栈为空的时候，
+ *          对栈调用pop()或者min()或者top()方法。
+ */
+public class Sooolution {
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
+    public void push(int node) {
+        stack1.push(node);
+        if(stack2.isEmpty()) {
+            stack2.push(node);
+        } else {
+            if(node < stack2.peek()) {
+                stack2.push(node);
+            } else {
+                stack2.push(stack2.peek());
+            }
+        }
+    }
+
+    public void pop() {
+        stack1.pop();
+        stack2.pop();
+    }
+
+    public int top() {
+        return stack1.peek();
+    }
+
+    public int min() {
+        return stack2.peek();
+    }
+}
